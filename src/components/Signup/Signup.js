1c8838fd754eab61f5bfca65cc0e2b641e3c27f9
Srinivas Tamada <srinivas@9lessons.info>
2017-10-10 09:17:18 -0400

    Signup Changes

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,20 +1,66 @@
 import React, {Component} from 'react';
-
+import {PostData} from '../../services/PostData';
+import {Redirect} from 'react-router-dom';
 import './Signup.css';
 
 class Signup extends Component {
+
+  constructor(props){
+    super(props);
+   
+    this.state = {
+     username: '',
+     password: '',
+     email: '',
+     name: '',
+     redirectToReferrer: false
+    };
+
+    this.signup = this.signup.bind(this);
+    this.onChange = this.onChange.bind(this);
+
+  }
+ 
+
+  signup() {
+    if(this.state.username && this.state.password && this.state.email && this.state.name){
+    PostData('signup',this.state).then((result) => {
+      let responseJson = result;
+      if(responseJson.userData){         
+        sessionStorage.setItem('userData',JSON.stringify(responseJson));
+        this.setState({redirectToReferrer: true});
+      }
+      
+     });
+    }
+  }
+
+ onChange(e){
+   this.setState({[e.target.name]:e.target.value});
+  }
+
   render() {
+    if (this.state.redirectToReferrer || sessionStorage.getItem('userData')) {
+      return (<Redirect to={'/home'}/>)
+    }
+   
+  
+
     return (
+      
       <div className="row " id="Body">
         <div className="medium-5 columns left">
         <h4>Signup</h4>
-        <label>Username</label>
-        <input type="text" name="username" placeholder="Username"/>
-        <label>Password</label>
-        <input type="password" name="password"  placeholder="Password"/>
         <label>Email</label>
-        <input type="password" name="password"  placeholder="Email"/>
-        <input type="submit" className="button" value="Sign Up"/>
+        <input type="text" name="name"  placeholder="Email" onChange={this.onChange}/>
+        <label>Name</label>
+        <input type="text" name="email"  placeholder="Name" onChange={this.onChange}/>
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Username" onChange={this.onChange}/>
+        <label>Password</label>
+        <input type="password" name="password"  placeholder="Password" onChange={this.onChange}/>
+        
+        <input type="submit" className="button" value="Sign Up" onClick={this.signup}/>
         <a href="/login">Login</a>
         </div>
         
